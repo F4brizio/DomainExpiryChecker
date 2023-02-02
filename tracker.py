f3c035a9952f356e73d1b184dfad75c2f82a66e0
Fabrizio <f4brizio.21@gmail.com>
2023-02-01 21:39:13 -0500

    fix text

--- tracker.py
+++ tracker.py
@@ -4,7 +4,7 @@ import tkinter as tk
 import winsound
 
 API_KEY = "token"
-DOMAIN = "doamin.com"
+DOMAIN = "domain.com"
 
 while True:
     response = requests.get(f"https://api.ip2whois.com/v2?key={API_KEY}&domain={DOMAIN}")
@@ -12,7 +12,7 @@ while True:
 
     if data["status"] == "error":
         root = tk.Tk()
-        root.title("Dominio liberado")
+        root.title("Domain Tracker")
         root.geometry("300x100")
         # center window
         root.update_idletasks()
@@ -21,12 +21,12 @@ while True:
         x = (root.winfo_screenwidth() // 2) - (width // 2)
         y = (root.winfo_screenheight() // 2) - (height // 2)
         root.geometry('{}x{}+{}+{}'.format(width, height, x, y))
-        label = tk.Label(root, text="El dominio ha expirado y ha sido liberado.")
+        label = tk.Label(root, text="The domain is available!")
         label.pack()
         winsound.PlaySound("SystemExclamation", winsound.SND_ALIAS)
         root.mainloop()
         break
     else:
-        print("El dominio aún está activo.")
+        print("The domain is still registered.")
 
     time.sleep(3600)
